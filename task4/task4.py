0766153df629712c14d505c096768c48c8f99669
mr2sia <Skuzovatov_2000@mail.ru>
2024-07-21 14:57:11 +0300

    Add files via upload

--- task4/task4.py
+++ task4/task4.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
     if len(sys.argv) >2 or  len(sys.argv) <2:
         print("Введите 1 аргумент")
     else:
-        a = [1, 10, 2, 9]
+        with open(sys.argv[1]) as f: a = f.readlines()
+        a = [int(i) for i in a]
         m = sorted(a)[len(a) // 2]
         print(sum(abs(v - m) for v in a))
